13fd33ebb50d385c738b5331cd3c9452c8644d61
senquevila <senquevila@gmail.com>
2026-07-15 15:28:02 -0600

    correccion de ultimo ejercicio

--- cap8/string_funciones2.c
+++ cap8/string_funciones2.c
@@ -4,13 +4,21 @@
 
 int main(int c, char *a[]) {
     int n1 = atoi(a[1]);
-    int n2 = strtol(a[1], (char**)NULL, 10);
-
     printf("atoi... %d\n", n1);
-    printf("strol... %d\n", n2);
 
+    errno = 0;
+    char *fin;
+    long n2 = strtol(a[1], &fin, 10);
+
+    
     if (errno != 0) {
-        printf("Hubo un error: %u", errno);
+        printf("Hubo un error: %u\n", errno);
+    } else if (fin == a[1] || *fin != '\0') {
+        printf("strtol: '%s' no es un numero valido\n", a[1]);
+        printf("fin: '%s'\n", fin);
+        printf("**fin: '%c'\n", *fin);
+    } else {
+        printf("strtol... %ld\n", n2);
     }
     return 0;
 }
